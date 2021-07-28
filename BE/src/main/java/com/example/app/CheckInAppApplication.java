bf7a236f3646e9686c569f4f7861d77b12ed0f3d
Brebeanu <a.brebeanu@student.tue.nl>
2021-07-28 12:41:51 +0300

    Initial commit

---
+++ BE/src/main/java/com/example/app/CheckInAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInAppApplication.class, args);
+	}
+
+}
